eb847e129739b62141d05825ae2c9d724f7e8395
danilkafish <fisher.de19@physics.msu.ru>
2025-10-09 13:27:22 +0300

    beta6.3

--- problems/quantum_circuits/for_test/main.py
+++
@@ -1,9 +0,0 @@
-from context import build_context
-from baseline import entrypoint
-from validate import validate
-
-if __name__ == "__main__":
-    context = build_context()
-    result = entrypoint(context)
-
-    print(validate((context,result)))
