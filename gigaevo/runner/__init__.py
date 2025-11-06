51dd947315e07994ef5e57403ec55f562eb59267
KhrulkovV <khrulkov.v@gmail.com>
2025-11-06 18:40:53 +0300

    fix: remove now unused runner

--- gigaevo/runner/__init__.py
+++ gigaevo/runner/__init__.py
@@ -1,3 +1,2 @@
 from gigaevo.runner.dag_blueprint import DAGBlueprint
 from gigaevo.runner.dag_runner import DagRunner, DagRunnerConfig, DagRunnerMetrics
-from gigaevo.runner.runner import EvolutionRunner
